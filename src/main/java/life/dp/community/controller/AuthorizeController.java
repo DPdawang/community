e1b4bb8099610f96264ef160dfdd2cd7b3aa0778
DPdawang <2464431410@qq.com>
2020-04-04 00:05:06 +0800

    update

---
+++ src/main/java/life/dp/community/controller/AuthorizeController.java
@@ -0,0 +1,43 @@
+package life.dp.community.controller;
+
+import life.dp.community.dataTransObj.AccessTokenDTO;
+import life.dp.community.dataTransObj.GithubUser;
+import life.dp.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@PropertySource({"classpath:application.properties"})
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client_id}")
+    private String clientId;
+    @Value("${github.client_secret}")
+    private String ClientSecret;
+    @Value("${github.redirect_uri}")
+    private String redirectUri;
+
+
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(ClientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
